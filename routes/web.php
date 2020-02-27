15d366ed3dab0a8f0614768903dbf3dd74f145fa
Hector Aguero <heagueron@gmail.com>
2020-02-27 15:12:30 -0400

    Cached clearing routes

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,28 @@ Route::get('/stweets/{stweet}', 'StweetController@show');
 Route::get('/stweets/{stweet}/edit', 'StweetController@edit');
 Route::patch('/stweets/{stweet}', 'StweetController@update');
 Route::delete('/stweets/stweetr}', 'StweetController@destroy');
+
+
+ //Clear route cache:
+ Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return 'Config cache cleared';
+}); 
+
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return 'View cache cleared';
+});
