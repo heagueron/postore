da58acb609fb81746e36063cd74d09b3fa0056a3
Hector Aguero <heagueron@gmail.com>
2020-02-27 15:45:48 -0400

    Creating tweet22

--- app/Http/Controllers/StweetController.php
+++ app/Http/Controllers/StweetController.php
@@ -26,7 +26,7 @@ class StweetController extends Controller
     {
         // twitter_profile_id forced. TODO: make it selectable.
         $user = \Auth::user();
-        dd($user);
+        // dd( $user, $user->twitter_profiles->first()->id );
         return view('stweets.create', compact('user'));
     }
 
