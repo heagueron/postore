d0afb79fc20f7c2add469ba8c60a67db6448b849
Hector Aguero <heagueron@gmail.com>
2020-12-02 12:11:30 -0400

    no referrer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="remote jobs">
     <meta name="author" content="Hector Aguero - heagueron@gmail.com">
+    <meta name="referrer" content="no-referrer">
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
