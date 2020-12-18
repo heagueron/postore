232b7913116feaf9c272eac9304dc0b6bbc82252
Hector Aguero <heagueron@gmail.com>
2020-12-18 10:02:14 -0400

    test language changes

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -3,8 +3,8 @@
 return [
 
     'pageTitle' => 'ADMIN OPTIONS',
-    'planPricesTitle'    => 'Plan Prices US$',
-    'otherParamsTitle'  => 'Other Parameters',
+    'planPricesTitle'    => 'Plan Prices in US$',
+    'otherParamsTitle'  => 'Other Admin Parameters',
     'update'    => 'Update',
 
 ];
