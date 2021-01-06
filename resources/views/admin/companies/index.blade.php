cfdcadf1318fd290ce6cd937fe83c5b72252995d
Hector Aguero <heagueron@gmail.com>
2021-01-06 14:15:38 -0400

    admin|added companies table

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -13,14 +13,38 @@
 
             <div class="d-flex justify-content-between mb-2">
 
-                <h2>COMPANIES</h2>
+                <h3>COMPANIES</h3>
 
                 {{-- Open Create Company Modal --}}
                 <button type="button" class="btn btn-success" data-toggle="modal" data-target="#createCompanyModal">
                     <i class="fas fa-plus-circle"></i>
                 </button>
-                
+
             </div>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Logo</th>
+                        <th>Company</th>
+                        <th>User</th>
+                        <th>RJs</th>
+                        <th>A RJs</th>
+                        <th>E</th>
+                        <th>D</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($companies as $company)
+                        <tr></tr>
+                        <tr>
+                            <td>{{ $company->name }}</td>
+                        </tr>
+                    @empty
+                        {{__('NO COMPANIES FOUND')}}
+                    @endforelse
+                </tbody>
+            </table>
 
         </div>
     </div>
