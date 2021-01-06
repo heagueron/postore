073963ad2240b50fb74e52c271475932f6a81743
Hector Aguero <heagueron@gmail.com>
2021-01-06 13:56:34 -0400

    added create company modal in admin

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -8,7 +8,19 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
          
-        Here will show companies ... 
+        {{-- MODALS --}}
+            @include('admin.companies.create')
+
+            <div class="d-flex justify-content-between mb-2">
+
+                <h2>COMPANIES</h2>
+
+                {{-- Open Create Company Modal --}}
+                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#createCompanyModal">
+                    <i class="fas fa-plus-circle"></i>
+                </button>
+                
+            </div>
 
         </div>
     </div>
