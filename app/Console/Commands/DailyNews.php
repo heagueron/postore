c2479dfc024d33504f9b1eebd49c35ef87132d3c
Hector Aguero <heagueron@gmail.com>
2021-02-10 16:25:54 -0400

    full command for all categories campaigns

--- app/Console/Commands/DailyNews.php
+++ app/Console/Commands/DailyNews.php
@@ -45,76 +45,83 @@ class DailyNews extends Command
         $api = Newsletter::getApi();
 
         // TESTING: MARKETING Remote Jobs
-        $category = \App\Category::where('tag', 'marketing')->first();
+        // $category = \App\Category::where('tag', 'marketing')->first();
 
-        if ( \App\Remjob::whereDate( 'created_at', '=', Carbon::yesterday()->toDateString() )
+        // Loop and prepare campaigns for every category
+
+        foreach (\App\Category::all() as $category) {
+
+            if ( \App\Remjob::whereDate( 'created_at', '=', Carbon::yesterday()->toDateString() )
                             ->where('category_id', $category->id)
                             ->exists() ){
-            $this->info( 'got ' .$category->tag. ' jobs.' );
+                $this->info( 'got ' .$category->tag. ' jobs.' );
             
-            // Replicate the campaigne
-            $clonCampaign = $api->post('campaigns/' .$category->mailchimp_base_campaign_id. '/actions/replicate');
+                // Replicate the campaigne
+                $clonCampaign = $api->post('campaigns/' .$category->mailchimp_base_campaign_id. '/actions/replicate');
 
-            if( $clonCampaign ) {
+                if( $clonCampaign ) {
 
-                $this->info('Campaign replication succeded. Category: '.$category->tag);
+                    $this->info('Campaign replication succeded. Category: '.$category->tag);
 
-                $campaignId = $clonCampaign["id"];
+                    $campaignId = $clonCampaign["id"];
 
-                $remjobs = \App\Remjob::whereDate( 'created_at', '=', Carbon::yesterday()->toDateString() )
-                            ->where('category_id', $category->id)->get();
-                
-                // Create the content
-                $html  = '<div style="background-color:#f2f5f3; padding:10px;">';
-                $html .= '<div style="background-color:#ffffff; padding:10px;">';
+                    $remjobs = \App\Remjob::whereDate( 'created_at', '=', Carbon::yesterday()->toDateString() )
+                                ->where('category_id', $category->id)->get();
+                    
+                    // Create the content
+                    $html  = '<div style="background-color:#f2f5f3; padding:10px;">';
+                    $html .= '<div style="background-color:#ffffff; padding:10px;">';
 
-                $html .= '<p>Hello</p>'; 
-                $html .= '<p>Here are your selected remote jobs for ' .Carbon::yesterday()->toFormattedDateString(). '. Click on any job title to get details.</p><br/>';
+                    $html .= '<p>Hello</p>'; 
+                    $html .= '<p>Here are your selected remote jobs. Click on any job title to get more details.</p><br/>';
 
-                $html .= '<hr style="display: block; margin-block-start: 0.5em; margin-block-end: 0.5em;"><br/>';
+                    $html .= '<hr style="display: block; margin-block-start: 0.5em; margin-block-end: 0.5em;"><br/>';
 
-                foreach ( $remjobs as $remjob ) {
-                    $html .= '<div style="line-height: 1.3;">';
-                    $html .= '<a style="text-decoration: none;" href="https://remjob.io/remote_job/' .$remjob->slug. '">';
-                    $html .= '<p style="color:#38c172;">' .$remjob->position. '</p></a>';
-                    $html .= '<p>At ' .$remjob->company->name. '</p>';
-                    $html .= '<p>[ ' .$remjob->locations. ' ]</p><br/>';
-                    $html .= '</div>';
-                }
-
-                $html .= '<hr style="display: block; margin-block-start: 0.5em; margin-block-end: 0.5em;"><br/>';
-
-                $html .= 'Find more jobs at: <a style="text-decoration: none;" href="https://remjob.io">https://remjob.io</a><br/>';
-
-                $html .= '<p>Have a nice day!</p><br/>';
-                $html .= '</div></div>';
-
-                //Update the clon campaigne
-                $options = [];
-                $updateResponse = Newsletter::updateContent( $campaignId, $html, $options);
-                if ( !$updateResponse  ) {
-
-                    $this->info('Campaign update failed for: '.$category->tag);
-
-                } else {
-
-                    // Send the campaign
-                    $response = $api->post('campaigns/' .$campaignId. '/actions/send');
-                    if( $response ) {
-                        $this->info('Campaign send initiated  for: '.$category->tag);
-                    } else {
-                        $this->info('Campaign send failed for: '.$category->tag);
+                    foreach ( $remjobs as $remjob ) {
+                        $html .= '<div style="line-height: 1.3;">';
+                        $html .= '<a style="text-decoration: none;" href="https://remjob.io/remote_job/' .$remjob->slug. '">';
+                        $html .= '<p style="color:#38c172;">' .$remjob->position. '</p></a>';
+                        $html .= '<p>At ' .$remjob->company->name. '</p>';
+                        $html .= '<p>[ ' .$remjob->locations. ' ]</p><br/>';
+                        $html .= '</div>';
                     }
 
+                    $html .= '<hr style="display: block; margin-block-start: 0.5em; margin-block-end: 0.5em;"><br/>';
+
+                    $html .= 'Find more jobs at: <a style="text-decoration: none;" href="https://remjob.io">https://remjob.io</a><br/>';
+
+                    $html .= '<p>Have a nice day!</p><br/>';
+                    $html .= '</div></div>';
+
+                    //Update the clon campaigne
+                    $options = [];
+                    $updateResponse = Newsletter::updateContent( $campaignId, $html, $options);
+                    if ( !$updateResponse  ) {
+
+                        $this->info('Campaign update failed for: '.$category->tag);
+
+                    } else {
+
+                        // Send the campaign
+                        $response = $api->post('campaigns/' .$campaignId. '/actions/send');
+                        if( $response ) {
+                            $this->info('Campaign send initiated  for: '.$category->tag);
+                        } else {
+                            $this->info('Campaign send failed for: '.$category->tag);
+                        }
+
+                    }
+
+                } else {
+                    $this->info('Campaign replication failed. Category: '.$category->tag);
                 }
 
             } else {
-                $this->info('Campaign replication failed. Category: '.$category->tag);
+                $this->info( 'There are no ' .$category->tag. ' jobs.' );
             }
-        } else {
-            $this->info( 'There are no ' .$category->tag. ' jobs.' );
-        }
-      
+
+        } //end cat foreach
+
 
     }
 
