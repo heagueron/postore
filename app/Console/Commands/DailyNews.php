5f9b2dfb1cca713bd042670d2b761ab5708b73e6
Hector Aguero <heagueron@gmail.com>
2021-02-23 13:43:42 -0400

    new daily mails improved

--- app/Console/Commands/DailyNews.php
+++ app/Console/Commands/DailyNews.php
@@ -72,7 +72,7 @@ class DailyNews extends Command
                     $html  = '<div style="margin-top:1rem;padding:25px;background-color:#f2f5f3;">';
 
                     // Content
-                    $html .= '<div style="background-color:#ffffff; padding:10px;">';
+                    $html .= '<div style="background-color:#ffffff; padding-left:20px;">';
 
                     $html .= '<p>Hello *|FNAME|*,</p>';  
                     $html .= '<p>Here are the latest <strong>'.$category->name.'</strong> remote jobs. Click on any job title to get more details.</p><br/>';
@@ -92,12 +92,12 @@ class DailyNews extends Command
 
                     $html .= '<hr style="display: block; margin-block-start: 0.5em; margin-block-end: 0.5em;"><br/>';
 
-                    $html .= '<p>If you have any questions or want to learn more about remjob.io, feel free to reach out to us at info@remjob.io.</p><br/>';
+                    $html .= '<p>If you have any questions or want to learn more about Remjob, feel free to reach out to us at <strong style="text-decoration: none;">info@remjob.io</strong></p><br/>';
 
-                    $html .= 'Find more jobs at: <a style="text-decoration: none;" href="https://remjob.io">https://remjob.io</a>';
+                    $html .= 'Find more jobs at: <a style="text-decoration: none;" href="https://remjob.io">https://remjob.io</a><br/>';
 
                     $html .= '<p>Cheers,</p>';
-                    $html .= '<p>Your friends at remjob.io</p><br/>';
+                    $html .= '<p>Your friends at Remjob</p><br/>';
                     $html .= '</div></div><br/>';
 
                     //Update the clon campaigne
