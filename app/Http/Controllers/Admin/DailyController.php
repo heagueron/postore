db08cc0b26a5376d0d3bd341adfc586e1592fc6a
Hector Aguero <heagueron@gmail.com>
2021-03-03 10:08:46 -0400

    refactor daily calc

--- app/Http/Controllers/Admin/DailyController.php
+++ app/Http/Controllers/Admin/DailyController.php
@@ -49,40 +49,38 @@ class DailyController extends Controller
                 break;
             }
 
-            //dd( Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'landing')->count() );
-            // Daily::create([
-            //     'track_day'         => $endDT->toDateString(),
-            //     'hits_landing'      => Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'landing')->count(),
-            //     'hits_details'      => Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'remjobs.show')->count(),
-            //     'hits_category'     => Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'remjobs.searchByTags')->count(),
-            //     'visitors_count'    => Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('first_on_date', 1)->count(),
-            // ]);
+            $landingVisits = Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'landing')->exists()
+                            ? Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'landing')->count()
+                            : 0;
 
+            $detailVisits = Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'remjobs.show')->exists()
+                            ? Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'remjobs.show')->count()
+                            : 0;
+                            
+            $categoryOrTagVisits = Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'remjobs.searchByTags')->exists()
+                            ? Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('entry_route', 'remjobs.searchByTags')->count()
+                            : 0;
 
-            $landingVisits = 0;
-            $detailVisits = 0;
-            $categoryOrTagVisits = 0;
-            $uniqueVisits = 0;
+            $uniqueVisitors = Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('first_on_date', 1)->exists()
+                            ? Visit::whereDate( 'created_at', '=', $endDT->toDateString())->where('first_on_date', 1)->count()
+                            : 0;
+            try{ 
 
-            foreach (Visit::all() as $visit) {
-                if ( Str::limit( $visit->created_at, 10, '') == $endDT->toDateString()  ) {
-                    if ( $visit->entry_route == 'landing'  ) { $landingVisits += 1; }
-                    if ( $visit->entry_route == 'remjobs.show'  ) { $detailVisits += 1; }
-                    if ( $visit->entry_route == 'remjobs.searchByTags'  ) { $categoryOrTagVisits += 1; }
-                    if ( $visit->first_on_date == '1'  ) { $uniqueVisits += 1; }
-                }
-            }
-            if ( $landingVisits + $detailVisits + $categoryOrTagVisits + $uniqueVisits > 0 ){
                 Daily::create([
-                    'track_day'     => $endDT->toDateString(),
-                    'hits_landing'  => $landingVisits,
-                    'hits_details'  => $detailVisits,
-                    'hits_category' => $categoryOrTagVisits,
-                    'visitors_count'=> $uniqueVisits,
+                    'track_day'         => $endDT->toDateString(),
+                    'hits_landing'      => $landingVisits,
+                    'hits_details'      => $detailVisits,
+                    'hits_category'     => $categoryOrTagVisits,
+                    'visitors_count'    => $uniqueVisitors,
                 ]);
-            }
 
-            $endDT->subDay();
+            } catch (\Exception $exception){ 
+                Log::info( 'Failed to create daily on: ' . $endDT->toDateString() );
+                dd( 'Failed to create daily on: ' . $endDT->toDateString(), $exception );
+            }
+            
+
+            $endDT->subDay();           
 
         }
 
