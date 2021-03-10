0efc4d53bc24d9bda8f6b5210c4dcc827769d332
Hector Aguero <heagueron@gmail.com>
2021-03-10 08:59:25 -0400

    ad in testMCC ad in div flex

--- app/Console/Commands/TestMCC.php
+++ app/Console/Commands/TestMCC.php
@@ -71,13 +71,15 @@ class TestMCC extends Command
                 $html .= '<div style="background-color:#ffffff; padding:15px">';
 
                 // Advertising
-                $html  = '<div style="border:1px solid #fbfbfb; margin-bottom:20px; line-height:1.3;>';
-                $html .= '<img src="https://remjob.io/images/ad_on_news_pure.png" alt="newsAd" style="width:100%;height: auto;">';
+                $html .= '<div style="border:1px solid #fbfbfb; margin-bottom:20px; line-height:1.3;>';
+                $html .= '<img src="https://remjob.io/images/ad_on_news_pure.png" alt="newsAd" style="width:100%;height:auto;">';
                 $html .= '<h3>Ad title</h3>';
                 $html .= '<p>Ad description. It will have a maximum of 50 words. Can be anything that explains your product or service.</p>';
-                $html .= '<a href="#">Ad link</a><br>';
-                $html .= '<small style="text-align:right;float:right;">Advertising</small>';
-                $html  = '</div>';
+                $html .= '<div style="display:flex;justify-content: space-between;">';
+                $html .= '<a href="#">Ad link</a>';
+                $html .= '<small>Advertising</small>';
+                $html .= '</div>';
+                $html .= '</div>';
 
                 // Greet
                 $html .= '<p>Hello *|FNAME|*,</p>'; 
