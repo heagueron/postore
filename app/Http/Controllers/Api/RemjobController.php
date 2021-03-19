015df7b06dbe9301912be6ddad469f652befa496
Hector Aguero <heagueron@gmail.com>
2021-03-19 08:59:36 -0400

    fix Visit | no auth in api

--- app/Http/Controllers/Api/RemjobController.php
+++ app/Http/Controllers/Api/RemjobController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Visit;
 use App\Remjob;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\RemjobResource;
@@ -24,7 +25,6 @@ class RemjobController extends Controller
     {
         // Register a visit
         $ip = \request()->ip();
-
         $this->registerVisit($ip, 'apiV1Remjobs');
 
         $remjobs = Remjob::where('active',1)
@@ -45,12 +45,7 @@ class RemjobController extends Controller
     {
         // Register a visit
         $ip = \request()->ip();
-        if( $ip != "127.0.0.1" AND $ip != "45.186.209.3" ) {
-
-            // Not administrator
-            $this->registerVisit($ip, 'apiV1RemjobsPro');
-
-        } 
+        $this->registerVisit($ip, 'apiV1Remjobs');
 
         $remjobs = Remjob::where('active',1)
             ->orderBy('created_at', 'desc')
@@ -60,7 +55,7 @@ class RemjobController extends Controller
     }
 
     private function registerVisit( $ip, $route, $firstOnDate = false ){
-
+        
         if( !Visit::where('visitor_ip', $ip)->exists() ){
             // First time ever!
             $firstOnDate = true;
@@ -75,7 +70,7 @@ class RemjobController extends Controller
         Visit::create([
             'visitor_ip'    => $ip,
             'entry_route'   => $route,
-            'user_id'       => Auth::check() ? Auth::user()->id : null,
+            'user_id'       => null,
             'first_on_date' => $firstOnDate
         ]);
 
@@ -83,69 +78,4 @@ class RemjobController extends Controller
         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Remjob  $remjob
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Remjob $remjob)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Remjob  $remjob
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Remjob $remjob)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Remjob  $remjob
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Remjob $remjob)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Remjob  $remjob
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Remjob $remjob)
-    {
-        //
-    }
 }
