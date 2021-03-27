8ddfceb9fad1e761cb3bf02d0cf37caaa1173ada
Hector Aguero <heagueron@gmail.com>
2021-03-27 15:34:29 -0400

    added laravel database scheduler

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-
 
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Gate::define('viewDatabaseSchedule', function ($user) {
+            return in_array($user->email, [
+                'heagueron@gmail.com',
+            ]);
+        });
 
     }
 
